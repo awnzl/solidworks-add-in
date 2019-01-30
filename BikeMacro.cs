8e90cdf506e41c3157fbff24f52654d90afafc4f
Alexander Wenzel <al.w.postbox@gmail.com>
2019-01-30 13:33:37 +0200

    solidworks taskpane add-in

---
+++ BikeMacro.cs
@@ -0,0 +1,154 @@
+﻿using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swpublished;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace solid_macro
+{
+    public class BikeMacro : ISwAddin
+    {
+        #region Private Members
+
+        /// <summary>
+        /// The cookie to the current instance of SW we are running inside of
+        /// </summary>
+        private int _swCookie;
+
+        /// <summary>
+        /// The current instance of SW
+        /// </summary>
+        private SldWorks _swApp;
+
+        /// <summary>
+        /// The taskplane view for our add-in
+        /// </summary>
+        private TaskpaneView _taskpaneView;
+
+        /// <summary>
+        /// The UI control that is going to be inside the SW taskpane view
+        /// </summary>
+        private TaskpaneHostUI _taskpaneHost;
+
+        #endregion
+
+        #region Public Members
+
+        /// <summary>
+        /// The ID to the taskpane used for registration in COM
+        /// </summary>
+        public const string PROGID = "SolidWorksBikeMacro";
+
+        #endregion
+
+        #region SW Add-in Callbacks
+
+        /// <summary>
+        /// Called when SW has loaded our add-in and wants us to do our
+        /// connection logic
+        /// </summary>
+        /// <param name="ThisSW">The current SW instance</param>
+        /// <param name="Cookie">The current SW cookie ID</param>
+        /// <returns></returns>
+        public bool ConnectToSW(object ThisSW, int Cookie)
+        {
+            // Store a reference to the current SW instance
+            _swApp = (SldWorks)ThisSW;
+
+            // Store cookie ID
+            _swCookie = Cookie;
+
+            // Setup callback info
+            var _var = _swApp.SetAddinCallbackInfo2(0, this, _swCookie);
+
+            // Create our UI
+            LoadUI();
+
+            return true;
+        }
+
+        /// <summary>
+        /// Called when SW has is about to unload our add-in and wants us to do
+        /// our disconnection logic
+        /// </summary>
+        /// <returns></returns>
+        public bool DisconnectFromSW()
+        {
+            // Clean up our UI
+            UnloadUI();
+            return true;
+        }
+
+        #endregion
+
+        #region Create UI
+
+        /// <summary>
+        /// Create our Taskpane and inject our host UI
+        /// </summary>
+        private void LoadUI()
+        {
+            var imgPath = Path.Combine(Path.GetDirectoryName(typeof(BikeMacro).Assembly.CodeBase).Replace(@"file:\", "")
+                                       , "bm_rd.png");
+
+            // create our taskpane
+            _taskpaneView = _swApp.CreateTaskpaneView2(imgPath, "BM");
+
+            // load oru UI into the taskpane
+            _taskpaneHost = (TaskpaneHostUI)_taskpaneView.AddControl(BikeMacro.PROGID, string.Empty);
+            _taskpaneHost.SWApp = _swApp;
+        }
+
+        /// <summary>
+        /// Cleanup the Taskpane when we disconnect
+        /// </summary>
+        private void UnloadUI()
+        {
+            _taskpaneHost = null;
+            _taskpaneView.DeleteView();
+
+            // Release COM reference and cleanup memory
+            Marshal.ReleaseComObject(_taskpaneView);
+            _taskpaneView = null;
+        }
+
+        #endregion
+
+        #region COM Registration
+
+        /// <summary>
+        /// The COM registration call to add our registry entries to the SW add-in registry
+        /// </summary>
+        /// <param name="t"></param>
+        [ComRegisterFunction()]
+        private static void ComRegister(Type t)
+        {
+            var keyPath = string.Format(@"SOFTWARE\SolidWorks\AddIns\{0:b}", t.GUID);
+
+            // Create our registry folder for the add-in
+            using (var rk = Microsoft.Win32.Registry.LocalMachine.CreateSubKey(keyPath))
+            {
+                // Load add-in when SW opens
+                rk.SetValue(null, 1);
+
+                // Setup SW add-in title and description
+                rk.SetValue("Title", "BikeMacro");
+                rk.SetValue("Description", "Bike test app");
+            }
+        }
+
+        /// <summary>
+        /// The COM unregister call to remove our custom entries we added in the COM register function
+        /// </summary>
+        /// <param name="t"></param>
+        [ComUnregisterFunction()]
+        private static void ComUnregister(Type t)
+        {
+            var keyPath = string.Format(@"SOFTWARE\SolidWorks\AddIns\{0:b}", t.GUID);
+
+            //Remove our registry entry
+            Microsoft.Win32.Registry.LocalMachine.DeleteSubKeyTree(keyPath);
+        }
+        #endregion
+    }
+}
---
+++ TaskpaneHostUI.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace solid_macro
+{
+    partial class TaskpaneHostUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.open_assembly_button = new System.Windows.Forms.Button();
+            this.execute_test_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // open_assembly_button
+            // 
+            this.open_assembly_button.Location = new System.Drawing.Point(10, 10);
+            this.open_assembly_button.Name = "open_assembly_button";
+            this.open_assembly_button.Size = new System.Drawing.Size(122, 40);
+            this.open_assembly_button.TabIndex = 0;
+            this.open_assembly_button.Text = "Open Bike Assembly";
+            this.open_assembly_button.UseVisualStyleBackColor = true;
+            this.open_assembly_button.Click += new System.EventHandler(this.OpenAssembly_Click);
+            // 
+            // execute_test_button
+            // 
+            this.execute_test_button.Enabled = false;
+            this.execute_test_button.Location = new System.Drawing.Point(10, 57);
+            this.execute_test_button.Name = "execute_test_button";
+            this.execute_test_button.Size = new System.Drawing.Size(122, 40);
+            this.execute_test_button.TabIndex = 1;
+            this.execute_test_button.Text = "Execute Bike Test";
+            this.execute_test_button.UseVisualStyleBackColor = true;
+            this.execute_test_button.Click += new System.EventHandler(this.BikeTest_Click);
+            // 
+            // TaskpaneHostUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.execute_test_button);
+            this.Controls.Add(this.open_assembly_button);
+            this.Name = "TaskpaneHostUI";
+            this.Size = new System.Drawing.Size(141, 197);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button open_assembly_button;
+        private System.Windows.Forms.Button execute_test_button;
+    }
+}
